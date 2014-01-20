592acfee2251b20caa8c5b0f7d6ce0025fdcc197
asi <asi@d7146f33-8f69-40ca-b591-b3d28f620eea>
2014-01-20 14:30:52 +0000

    add File B.php
    git-svn-id: http://10.241.1.192:800/repos/TestBranch/Trunk@4394 d7146f33-8f69-40ca-b591-b3d28f620eea

---
+++ File B.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <body>
+<?php
+$a=array("Volvo"=>"XC90","BMW"=>"X5");
+if (array_key_exists("Volvo",$a))
+   {
+   echo "Hi, Key exists!";
+   }
+else
+   {
+   echo "Woo, Key does not exist!";
+   }
+    ?>
+  </body>
+</html>
