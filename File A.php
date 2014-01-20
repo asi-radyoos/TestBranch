fb4a34256835ff78987ba8507dfe62aa62dd51e2
asi <asi@d7146f33-8f69-40ca-b591-b3d28f620eea>
2014-01-20 14:08:46 +0000

    Indent a
    git-svn-id: http://10.241.1.192:800/repos/TestBranch/Trunk@4392 d7146f33-8f69-40ca-b591-b3d28f620eea

--- File A.php
+++ File A.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
-<html>
-<body>
-
+<html>  
+  <body>
 <?php
 $txt1="Learn PHP Now";
 $txt2="W3Schools.com";
@@ -11,7 +10,6 @@ echo "<br>";
 echo "Study PHP at $txt2";
 echo "<br>";
 echo "My car brand is; {$cars[0]}";
-?>
-
-</body>
+        ?>  
+  </body>
 </html>
