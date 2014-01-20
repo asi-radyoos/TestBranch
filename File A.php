8e5562b59abb0ee2db775ce3b9624e4da7eff510
asi <asi@d7146f33-8f69-40ca-b591-b3d28f620eea>
2014-01-20 12:31:31 +0000

    git-svn-id: http://10.241.1.192:800/repos/TestBranch/Trunk@4382 d7146f33-8f69-40ca-b591-b3d28f620eea

---
+++ File A.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1="Learn PHP Now";
+$txt2="W3Schools.com";
+$cars=array("Volvo","BMW","Toyota");
+echo $txt1;
+echo "<br>";
+echo "Study PHP at $txt2";
+echo "<br>";
+echo "My car brand is; {$cars[0]}";
+?>
+
+</body>
+</html>
