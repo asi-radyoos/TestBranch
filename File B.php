2fbe4b374468043ee674b22cf1c0b44d1f9999fd
asi <asi@d7146f33-8f69-40ca-b591-b3d28f620eea>
2014-01-21 06:46:40 +0000

    Add Saab
    git-svn-id: http://10.241.1.192:800/repos/TestBranch/Trunk@4400 d7146f33-8f69-40ca-b591-b3d28f620eea

--- File B.php
+++ File B.php
@@ -2,7 +2,7 @@
 <html>
   <body>
 <?php
-$a=array("Volvo"=>"XC90","BMW"=>"X5");
+$a=array("Volvo"=>"XC90","BMW"=>"X5","Saab"=>"71");
 if (array_key_exists("Volvo",$a))
    {
    echo "Hi, Key exists!";
