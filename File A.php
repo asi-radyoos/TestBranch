0fc57ca5b9e560bd0ede79306c400a6827d9df2c
asi-radyoos <asi@radyoos.com>
2014-02-09 10:35:44 +0200

    add !!

--- File A.php
+++ File A.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>  
   <body>
-  <!-Comment Radyoos 2014->
+  <!-Comment Radyoos 2014 !!->
 <?php
 $txt1="Learn PHP Now!";
 $txt2="JollyWollet.com The Best";
 $cars=array("Volvo","BMW","Toyota","Asics","SaaB", "Fiat");
 echo $txt1;
 echo "<br>";
-echo "Radyoos 2014";
+echo "Radyoos 2014 !!";
 echo "<br>";
 echo "Study PHP at $txt2 Continue to work";
 echo "<br>";
